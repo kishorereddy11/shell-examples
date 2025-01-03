58f8a07912378048767e5a5cdccfd394f5c5fd58
kishor <kishorebsc12@gmail.com>
2025-01-03 12:36:32 +0530

    example

--- arrays.sh
+++ arrays.sh
@@ -2,4 +2,6 @@
 Movies=("puspa","rrr","devara")
 #index is 2 and size: 3
 
-echo "Last movie ${Movies[0]}"
+echo "first movie ${Movies[0]}"
+echo "second movie ${Movies[1]}"
+echo "third movie ${Movies[2]}"
