89797891051a4213ab02271096440117583df743
kishor <kishorebsc12@gmail.com>
2025-01-03 11:20:48 +0530

    example

--- inputvariables.sh
+++ inputvariables.sh
@@ -1,19 +1,5 @@
-# #!/bin/bash
-# echo "Enter username :"
-# read -p USERNAME
-# echo "\nEntered username $USERNAME"
-# echo "Enter the password :"
-# read -sp PASSWORD
-# echo "\nEntered password $PASSWORD"
-
 #!/bin/bash
 
-echo "Please enter your username::" # text entered here will be added as value to variable
-
-read -s USERNAME # Here USERNAME is the variable name
-
-echo "username entered: $USERNAME"
-
-echo "Please enter your password::"
-
-read -s PASSWORD
+echo "Enter username : "
+read USERNAME
+echo "$USERNAME"
