07da3891751cac1debd4842491d39dab10af3411
kishor <kishorebsc12@gmail.com>
2025-01-03 12:38:05 +0530

    example

--- arrays.sh
+++ arrays.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-Movies=("puspa","rrr","devara")
+MOVIES=("puspa","rrr","devara")
 #index is 2 and size: 3
 
-echo "first movie ${Movies[0]}"
-echo "second movie ${Movies[1]}"
-echo "third movie ${Movies[2]}"
+echo "first movie ${MOVIES[0]}"
+echo "second movie ${MOVIES[1]}"
+echo "third movie ${MOVIES[2]}"
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
