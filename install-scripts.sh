d835717bf91ff2262acbe26835a75336add6bf09
kishor <kishorebsc12@gmail.com>
2025-01-05 10:31:13 +0530

    example

--- install-scripts.sh
+++ install-scripts.sh
@@ -3,6 +3,26 @@ uname=$(id -u)
 if [ $uname -eq 0]
 then 
 echo "current user is root user"
+install()
 else
-echo "current user is not a root user"
+exit 1
 fi
+
+validate() {
+ if [ $1 -eq 0] 
+ then 
+ echo "$2 Sucess"
+ else
+ echo "$2 Failure"
+}
+
+
+install(){
+    dnf list installed mysql
+    if [ $? -eq 0]
+    then
+    dnf install mysql -y
+    validate $? "installing mysql"
+    else
+    validate $? "already present mysql"
+}
