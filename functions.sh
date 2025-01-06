70ba055d1554a6f4b3d9bdc14267380c8c8be408
kishor <kishorebsc12@gmail.com>
2025-01-06 21:00:22 +0530

    example

--- functions.sh
+++ functions.sh
@@ -2,9 +2,27 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+echo "$2 Failure"
+else
+echo "$2 Success"
+fi
+}
+
 if [ $USERID -ne 0 ] 
 then
 echo "please login as root user to execute this script"
 exit 1
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+else
+echo "already installed mysql "
+fi
