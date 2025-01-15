818afa9ee7aea011b053ba29d6f8b203b97bde27
kishor <kishorebsc12@gmail.com>
2025-01-15 11:10:45 +0530

    example

--- 13-logs.sh
+++ 13-logs.sh
@@ -6,6 +6,12 @@ G="\e[32m"
 Y="\e[33m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 |cut -d "." f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP.log"
+
+echo "Script started at $TIMESTAMP" &>> $LOG_FILE_NAME
 
 if [ -d $LOGS_FOLDER ]
 then
@@ -26,17 +32,19 @@ VALIDATE(){
     fi
 }
 
+ 
+
 if [ $USERID -ne 0 ] 
 then
     echo -e  "$R please use root user to execute this script"
     exit 1
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>> $LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then 
-    dnf install mysql -y
+    dnf install mysql -y &>> $LOG_FILE_NAME
     VALIDATE "$?" "Installing mysql"
 else
 echo -e "Mysql is already ... $Y installed"
