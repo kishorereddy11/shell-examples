009c441dce7976ffde2f41a4094b9ebf4ae43e5b
kishor <kishorebsc12@gmail.com>
2025-01-15 11:25:40 +0530

    example

--- 13-logs.sh
+++ 13-logs.sh
@@ -12,7 +12,7 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP.log"
 
-echo "Script started at $TIMESTAMP" &>> $LOG_FILE_NAME
+echo "Script started at $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ -d $LOGS_FOLDER ]
 then
@@ -41,11 +41,11 @@ then
     exit 1
 fi
 
-dnf list installed mysql &>> $LOG_FILE_NAME
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then 
-    dnf install mysql -y &>> $LOG_FILE_NAME
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE "$?" "Installing mysql"
 else
 echo -e "Mysql is already ... $Y installed"
